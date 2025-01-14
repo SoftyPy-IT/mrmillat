eb53249df2066ca2ba0dbf46ada418e1a84c0694
MD. SHIPON <sshimi910@gmail.com>
2025-01-13 16:48:58 -0800

    layout.tsx page meta data change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,34 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
 export const metadata: Metadata = {
-  title: {
-    default: "M. R. Millat",
-    template: "%s - M. R. Millat",
+  title: "Softypy - Trusted e-solutions",
+  description:
+    "Softypy is a trusted e-solutions provider. We provide services like web development, mobile app development, digital marketing, and more. Contact us now! ",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://softypy.com",
+    siteName: "Softypy - Trusted e-solutions",
+    title: "Softypy - Trusted e-solutions",
+    description:
+      "Softypy is a trusted e-solutions provider. We provide services like web development, mobile app development, digital marketing, and more. Contact us now! ",
+    images: [
+      {
+        url: "/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Softypy",
+      },
+    ],
+    countryName: "Bangladesh",
+    emails: ["softypy@gmail.com"],
+    phoneNumbers: ["+880 1762-380594"],
   },
-  description: "Ex. MP Jamalpur-1",
 };
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
