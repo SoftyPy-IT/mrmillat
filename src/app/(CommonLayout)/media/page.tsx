f2d13486079384cfe5eef95ca242b2b1824f6b02
AlMamunTalukder <almamuntalukder0@gmail.com>
2025-12-28 16:28:56 +0600

    update code for video show in details page

--- src/app/(CommonLayout)/media/page.tsx
+++ src/app/(CommonLayout)/media/page.tsx
@@ -125,7 +125,7 @@ const Media = () => {
     const getData = async () => {
       try {
         const response = await axiosPublic.get(
-          `videos?limit=${limit}&page=${currentPage}`
+          `voice-on-media?limit=${limit}&page=${currentPage}`
         );
         const { totalCount, data } = response?.data?.data;
         setMedias(data);
